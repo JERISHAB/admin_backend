6cc898a7954f0d79a33293be8ea5e15ffc5317a3
JERISHAB <jerishabmayan@gmail.com>
2025-02-10 22:23:57 +0530

    feat: Add member routes and include in main API router

--- app/api/v1/routes/__init__.py
+++ app/api/v1/routes/__init__.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 from app.api.v1.routes import auth
 from app.api.v1.routes import job
+from app.api.v1.routes import member
 
 
 router = APIRouter()
 
 router.include_router(auth.router, prefix="/auth", tags=["auth"])
 router.include_router(job.router, prefix="/jobs", tags=["jobs"])
+router.include_router(member.router, prefix="/members", tags=["members"])
--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import os
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.v1.routes import auth,job, router as api_router  # Import your API routes
+from app.api.v1.routes import auth,job,member, router as api_router  # Import your API routes
 
 # Load environment variables from a .env file
 load_dotenv()
@@ -19,9 +19,10 @@ app.add_middleware(
     allow_headers=["*"],  # Allow all headers
 )
 
-# Include API routers for different routes (e.g., users, auth)
+# Include API routers for different routes
 app.include_router(auth.router, prefix="/api/v1/users", tags=["Users"])  # Users-related routes
 app.include_router(job.router, prefix="/api/v1/jobs", tags=["Jobs"])  # Jobs-related routes
+app.include_router(member.router, prefix="/api/v1/members", tags=["Members"])  # Members-related routes
 # Root endpoint for basic health check
 
 @app.get("/")
