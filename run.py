1a39432ef41fbfac662552e7631f39056349303b
JERISHAB <jerishabmayan@gmail.com>
2025-03-07 23:02:31 +0530

    feat: enable migration execution on script run

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ def run_migrations():
         exit(1)
 
 if __name__ == "__main__":
-    #run_migrations()
+    run_migrations()
 
     port = int(os.getenv("PORT", 8000))  # Default to 8000 if PORT is not set
     host = os.getenv("HOST","127.0.0.1")  # Default to 0.0.0.0 for external access
